51fc2946567042453674d5e2f5d490d66cacd695
Mason <mason.d.mcmanus@outlook.com>
2024-05-02 18:10:40 -0500

    Added auto selection

--- src/client.js
+++ src/client.js
@@ -3,38 +3,94 @@ var socket = io();
 
 var tab = "tele";
 
+// TODO CHANGE THIS FOR NEW AUTOS
+var autoRoutines = ["Source 4-5", "Subwoofer 6-7-8", "Subwoofer 1-2-3", "Subwoofer 5-6"];
+
 // Socket listeners
 socket.on('update', handleUpdate);
 
 // Shared elements
 const connectStatusContainer = document.getElementById("connect-status-container");
 
-var conditionalElements = {
-    matchTimeText: document.getElementById("game-time-text")
+var teleElements = {
+    container: document.getElementById("tele-dashboard-container"),
+    matchTimeText: document.getElementById("game-time-text"),
+    intakeStateText: document.getElementById("intake-state-text")
 }
+
+var autoElements = {
+    container: document.getElementById("auto-dashboard-container"),
+    select: document.getElementById("auto-select"),
+    blinkButton: document.getElementById("auto-blink-button"),
+    autoRecieved: document.getElementById("auto-recieve-text")
+}
+
+var headerElements = {
+    autoButton: document.getElementById("auto-button"),
+    teleButton: document.getElementById("tele-button")
+}
+
+autoElements.select.addEventListener("change", switchAuto);
 
 // Update handler
 function handleUpdate(args) {
     args.connected ? connected() : disconected();
 
+    
     if (tab == "tele") {
+        
+        // Match time
         let matchTime = Math.round(args.matchTime - 0.675);
-
-        conditionalElements.matchTimeText.innerText = `${args.matchTime <= -1 ? "..." : Math.round(args.matchTime - 0.675)}`;
+        console.log(matchTime);
+        teleElements.matchTimeText.innerText = `${args.matchTime <= -1 ? "..." : Math.round(args.matchTime - 0.675)}`;
         if (matchTime >= -1) {
             if (matchTime <= 30) {
-                conditionalElements.matchTimeText.style.backgroundColor = "red";
+                teleElements.matchTimeText.style.backgroundColor = "red";
             } else if (matchTime <= 45) {
-                conditionalElements.matchTimeText.style.backgroundColor = "rgb(255, 115, 0)";
+                teleElements.matchTimeText.style.backgroundColor = "rgb(255, 115, 0)";
             } else if (matchTime <= 75) {
-                conditionalElements.matchTimeText.style.backgroundColor = "yellow";
+                teleElements.matchTimeText.style.backgroundColor = "yellow";
             } else {
-                conditionalElements.matchTimeText.style.backgroundColor = "lime";
+                teleElements.matchTimeText.style.backgroundColor = "darkgreen";
             }
         }
 
-    } else {
+        // Intake detection
+        if (args.pieceDetected && matchTime >= -1) {
+            let elems = document.body.getElementsByTagName("*");
+            for (let i = 0; i < elems.length; i++) {
+                elems[i].classList.add("green-alert");
+            }
+        } else {
+            let elems = document.body.getElementsByTagName("*");
+            for (let i = 0; i < elems.length; i++) {
+                elems[i].classList.remove("green-alert");
+            }
+        }
+        if (matchTime >= -1 && matchTime <= 30) {
+            let elems = document.body.getElementsByTagName("*");
+            for (let i = 0; i < elems.length; i++) {
+                elems[i].classList.add("red-alert");
+            }
+        } else {
+            let elems = document.body.getElementsByTagName("*");
+            for (let i = 0; i < elems.length; i++) {
+                elems[i].classList.remove("red-alert");
+            }
+        }
+        teleElements.matchTimeText.classList = "";
 
+        // Intake state
+        if (args.intakeDeployed && matchTime >= -1) {
+            teleElements.intakeStateText.innerText = "Intake\nDeloyed";
+            teleElements.intakeStateText.className = "deployed-warning";
+        } else {
+            teleElements.intakeStateText.innerText = "Intake\nRetracted";
+            teleElements.intakeStateText.className = "";
+        }
+
+    } else {
+        autoElements.autoRecieved.innerText = `Recieved ${args.autoRoutine}.`
     }
 }
 
@@ -46,4 +102,37 @@ function disconected() {
 function connected() {
     connectStatusContainer.innerText = "Connected to robot.";
     connectStatusContainer.style.backgroundColor = "rgb(23, 179, 36)";
+}
+
+function updateAutoSelection() {
+    socket.emit("update_auto_selection");
+}
+
+function showAuto() {
+    teleElements.container.style.display = "none";
+    autoElements.container.style.display = "grid";
+    headerElements.teleButton.classList.remove("active");
+    headerElements.autoButton.classList.add("active");
+    tab = "auto";
+}
+
+function showTele() {
+    autoElements.container.style.display = "none";
+    teleElements.container.style.display = "grid";
+    headerElements.autoButton.classList.remove("active");
+    headerElements.teleButton.classList.add("active");
+    tab = "tele";
+}
+
+showAuto();
+switchAuto();
+
+function switchAuto() {
+    let blinkNumber = autoRoutines.indexOf(autoElements.select.value) + 2;
+    autoElements.blinkButton.innerText = `${blinkNumber} Blinks`;
+    socket.emit("update_auto_selection", autoElements.select.value);
+}
+
+function sendBlink() {
+    socket.emit("auto_blink_test", 0);
 }
--- src/main.js
+++ src/main.js
@@ -21,9 +21,12 @@ server.listen(PORT, function () {
 // Dashboard values
 var dashboardValues = {
   matchTime: 0,
-  connected: false
+  connected: false,
+  intakeDeployed: false,
+  pieceDetected: false,
+  autoRoutine: 'Source 4-5'
 }
-
+var blinkID = 0;
 
 
 
@@ -31,19 +34,22 @@ const socketio = new Server(server);
 
 function update() {
   dashboardValues.connected = ntcore.isRobotConnected();
+  dashboardValues.autoRoutine = autoModeTopic.getValue();
   socketio.emit('update', dashboardValues);
   //console.log(dashboardValues.Rot)
 }
 
 socketio.on("connection", (socket) => {
   socket.on("refresh", tryConnect);
+  socket.on("update_auto_selection", updateAutoSelection);
+  socket.on("auto_blink_test", sendBlinkRequest);
 });
 
 setInterval(update, 100);
 
 //TODO: Just for debugging, change to team number for default
 // Network table things
-const ntcore = NetworkTables.getInstanceByURI('127.0.0.1');
+const ntcore = NetworkTables.getInstanceByURI('3197');
 
 // Subscriptions
 const matchTimeTopic = ntcore.createTopic('/SmartDashboard/match_time', NetworkTablesTypeInfos.kDouble);
@@ -51,10 +57,39 @@ matchTimeTopic.subscribe((value) => {
   dashboardValues.matchTime = value;
 }, true);
 
+const intakeStateTopic = ntcore.createTopic('/SmartDashboard/intake_deployed', NetworkTablesTypeInfos.kBoolean);
+intakeStateTopic.subscribe((value) => {
+  dashboardValues.intakeDeployed = value;
+}, true);
+
+const pieceDetectTopic = ntcore.createTopic('/SmartDashboard/piece_detected', NetworkTablesTypeInfos.kBoolean);
+pieceDetectTopic.subscribe((value) => {
+  dashboardValues.pieceDetected = value;
+}, true);
+
+// Publishers
+const autoModeTopic = ntcore.createTopic('/Auto/autoMode', NetworkTablesTypeInfos.kString, 'No Auto');
+await autoModeTopic.publish();
+updateAutoSelection('Source 4-5');
+
+const blinkTestTopic = ntcore.createTopic('/Auto/blinkID', NetworkTablesTypeInfos.kInteger, 0);
+await blinkTestTopic.publish();
+blinkTestTopic.setValue(0);
+
+
 function tryConnect(address) {
   if (address == "" || address == null) {
     return;
   }
 
   ntcore.changeURI(address);
+}
+
+function updateAutoSelection(value) {
+  autoModeTopic.setValue(value);
+}
+
+function sendBlinkRequest(value) {
+  blinkID ++;
+  blinkTestTopic.setValue(blinkID);
 }
